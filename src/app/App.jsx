6d1349497ca6dcbb0c334a05e88b78c32400875f
DevpegAr <pgrilletti@gmail.com>
2025-11-24 12:39:34 -0300

    Se agrega mejoras y componente scroll to top

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,13 @@
 // src/app/App.jsx
 import MainLayout from "../components/layout/MainLayout.jsx";
 import Home from "../pages/Home/Home.jsx";
+import ScrollTopButton from "../components/ScrollToTop/ScrollTopButton.jsx";
 
 export default function App() {
   return (
     <MainLayout>
       <Home />
+      <ScrollTopButton />
     </MainLayout>
   );
 }
